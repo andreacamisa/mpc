a5e2d022b77100e1c5cada0d699452c456b901f8
Andrea Camisa <andrea.camisa92@gmail.com>
2023-08-03 13:48:09 +0000

    use single indexing

--- src/mpc/example.py
+++ src/mpc/example.py
@@ -33,28 +33,24 @@ R = gen_symm_pos_def_matrix(nu)
 P = np.empty((N, nx, nx))
 x = np.empty((N, nx))
 u = np.empty((N, nu))
-x[0, :] = x0
+x[0] = x0
 
 # backward pass
-P[-1, :, :] = Q
+P[-1] = Q
 for k in reversed(range(N - 1)):
-    tmp = A.T @ P[k + 1, :, :] @ B
-    P[k, :, :] = (
-        Q
-        + A.T @ P[k + 1, :, :] @ A
-        - tmp @ np.linalg.inv(R + B.T @ P[k + 1, :, :] @ B) @ tmp.T
-    )
+    tmp = A.T @ P[k + 1] @ B
+    P[k] = Q + A.T @ P[k + 1] @ A - tmp @ np.linalg.inv(R + B.T @ P[k + 1] @ B) @ tmp.T
 
 # forward pass
 for k in range(N - 1):
-    K = -np.linalg.inv(R + B.T @ P[k + 1, :, :] @ B) @ (B.T @ P[k + 1, :, :] @ A)
-    u[k, :] = K @ x[k, :]
-    x[k + 1, :] = A @ x[k, :] + B @ u[k, :]
+    K = -np.linalg.inv(R + B.T @ P[k + 1] @ B) @ (B.T @ P[k + 1] @ A)
+    u[k] = K @ x[k]
+    x[k + 1] = A @ x[k] + B @ u[k]
 
 print("Solution:")
 
 for k in range(N):
     print(f"time {k}")
-    print(f"\tx = {np.array2string(x[k, :], precision=4, floatmode='fixed')}")
+    print(f"\tx = {np.array2string(x[k], precision=4, floatmode='fixed')}")
     if k != N - 1:
-        print(f"\tu = {np.array2string(u[k, :], precision=4, floatmode='fixed')}")
+        print(f"\tu = {np.array2string(u[k], precision=4, floatmode='fixed')}")
