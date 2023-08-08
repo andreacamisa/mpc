cb4f68ff1e0aea8ac7d7fc19106a060168c2880d
Andrea Camisa <andrea.camisa92@gmail.com>
2023-08-08 22:33:54 +0000

    add docstrings to optimal control problem

--- src/mpc/problem.py
+++ src/mpc/problem.py
@@ -7,7 +7,17 @@ from nptyping import Float, NDArray, Shape
 
 @dataclass(frozen=True)
 class OptimalControlProblem:
+    """Finite-horizon optimal control problem composed of cost function,
+    system dynamics and initial state."""
+
     horizon: int
+    """Prediction horizon of optimal control problem."""
+
     cost: Cost
+    """Cost function appearing of optimal control problem."""
+
     system: System
+    """System dynamics."""
+
     initial_state: NDArray[Shape["N"], Float]
+    """Initial state of the system."""
